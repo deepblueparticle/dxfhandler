2048bfbb69752e9c24f62b88670efb8f96f0b8c3
Fernando <ferlopezcardenas@gmail.com>
2013-06-28 12:24:12 -0600

    reorder

--- src/dxfhandle.h
+++ src/dxfhandle.h
@@ -1,37 +1,17 @@
 /*
-*	Implementation of dxliib for DXF handling
+*	Implementation of dxlib for DXF handling
 *	Autor : FLopez
 */
 
 #ifndef DXFHANDLE_H
 #define DXFHANDLE_H
+#include <string>
 
-#include "3rdParty/dxflib/src/dl_writer_ascii.h"
-#include "3rdParty/dxflib/src/dl_dxf.h"
-// </Includes>
+namespace WriteDxf{
+	public:
+		static void WriteLine(string fileName, float[4] coordinates);
 
-class DxfExport;
-class DxfImport;
 
-class DxfHandle : public DL_creationAdapter{
-	virtual void addLine(const DL_LineData& line);
-	virtual void createDxfFile();
-
-	void DxfHandle::addLine(const DL_LineData& linea){
-
-	}
-
-	int DxfHandle::createDxfFile(){
-		DL_Dxf dxf;
-		DL_Codes::version exportVersion = DL_Codes::AC1015;
-		DL_WriterA* dw = dxf.out("archivoNuevo.dxf", exportVersion);
-		if(dw==NULL){
-			printf("%s\n Cannot open 'archivoNuevo.dxf for writing");
-			return 0;
-		}
-		return 1;
-	}
-
-};
+}
 
 #endif
